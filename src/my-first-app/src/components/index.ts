51e8067da5cbb1679a8c676241d315eba997a9f8
itskritix <itskritix@gmail.com>
2026-01-15 08:46:27 +0530

    chore: Remove test files

--- src/my-first-app/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { ErrorBoundary } from "./ErrorBoundary";
-export { ToastProvider, useToast } from "./Toast";
-export { KeyboardAware } from "./KeyboardAware";
-export { OfflineBanner } from "./OfflineBanner";
